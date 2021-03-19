6d8194caea6da95a651b72d0dbc99aaab2dc14c5
Efua Akumanyi <efua.akumanyi@gmail.com>
2021-03-19 20:20:28 +0000

    Local super-linter changes applied

--- src/App.js
+++ src/App.js
@@ -1,81 +1,81 @@
-import React, { useState } from 'react';
-import Game from './components/game';
-import gameData from './models/dt.json';
+import React, { useState } from 'react'
+import Game from './components/game'
+import gameData from './models/dt.json'
 
-const MAX_MATCH = 2;
+const MAX_MATCH = 2
 
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-  while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+function shuffle (array) {
+  let currentIndex = array.length; let temporaryValue; let randomIndex
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
   }
-  return array;
+  return array
 }
 
-function App() {
-  const [pair, updatePair] = useState([]);
-  const [cards, updateCards] = useState(shuffle(gameData.cards));
+function App () {
+  const [pair, updatePair] = useState([])
+  const [cards, updateCards] = useState(shuffle(gameData.cards))
 
-  function refreshCardState(currentCards, tuple, state){
+  function refreshCardState (currentCards, tuple, state) {
     return [...currentCards.map(card => {
       tuple.forEach(item => {
-        if(item.id === card.id){
-          card.state =state;
+        if (item.id === card.id) {
+          card.state = state
         }
-      });
-      return card;
-    })];
+      })
+      return card
+    })]
   }
-  
+
   const flipCard = (id) => {
-    let currentPair = pair;
-    let currentCards = cards;
-    let currentCard = currentCards.find(card => card.id === id);
+    let currentPair = pair
+    let currentCards = cards
+    const currentCard = currentCards.find(card => card.id === id)
 
-    if(currentCard.state!=="hidden"){
-      return;
+    if (currentCard.state !== 'hidden') {
+      return
     }
 
-    if(currentPair.length === MAX_MATCH){
-      currentCards = refreshCardState(currentCards,pair,"hidden");
-      currentPair = [];
+    if (currentPair.length === MAX_MATCH) {
+      currentCards = refreshCardState(currentCards, pair, 'hidden')
+      currentPair = []
     }
 
-    let tempPair = [...currentPair,currentCard];
-    if(currentPair.length < MAX_MATCH) {
-      if(currentCard.state === "hidden"){
-        currentCards = refreshCardState(currentCards, tempPair,`show${tempPair.length === MAX_MATCH?" max":""}`);
-        currentPair = tempPair;
+    const tempPair = [...currentPair, currentCard]
+    if (currentPair.length < MAX_MATCH) {
+      if (currentCard.state === 'hidden') {
+        currentCards = refreshCardState(currentCards, tempPair, `show${tempPair.length === MAX_MATCH ? ' max' : ''}`)
+        currentPair = tempPair
       }
-        
-      if(tempPair.length === MAX_MATCH) {
-        const [first,second] = tempPair;
-        if(first.matchid === second.matchid){
-          currentCards = refreshCardState(currentCards, tempPair,"matched");
-          currentPair = [];
+
+      if (tempPair.length === MAX_MATCH) {
+        const [first, second] = tempPair
+        if (first.matchid === second.matchid) {
+          currentCards = refreshCardState(currentCards, tempPair, 'matched')
+          currentPair = []
         }
-          // else{
-          //   const timer = setTimeout(() => {
-          //   currentCards = refreshCardState(currentCards,pair,"hidden");
-          //     updatePair([]);
-          //   }, gameData.flipSpeed);
-          //   return () => clearTimeout(timer);
-          // }
+        // else{
+        //   const timer = setTimeout(() => {
+        //   currentCards = refreshCardState(currentCards,pair,"hidden");
+        //     updatePair([]);
+        //   }, gameData.flipSpeed);
+        //   return () => clearTimeout(timer);
+        // }
       }
     }
-    updatePair(currentPair);
-    updateCards(currentCards);
+    updatePair(currentPair)
+    updateCards(currentCards)
   }
 
   return (
-    <div className="App">
+    <div className='App'>
       <Game name={gameData.name} folder={gameData.folder} rows={gameData.rows} cards={cards} flipCard={flipCard} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/card.js
+++ src/components/card.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-const Card = ({card, ...props}) => {
- 
-    return (
-      <div className={`card ${card.state}`} onClick={() => props.flipCard(card.id)}>
-        <div className="inner">
-          <div className="front"></div>
-          <div className="back">
-            {card.hasOwnProperty("img")?
-            <img src={`./img/${props.folder}/${card.img}`} className={`card-img`} alt={card.img}/>:
-            <div className={`card-txt`}><h2>{card.txt}</h2><p>{card.helpTxt}</p></div>}
-          </div>
+const Card = ({ card, ...props }) => {
+  return (
+    <div className={`card ${card.state}`} onClick={() => props.flipCard(card.id)}>
+      <div className='inner'>
+        <div className='front' />
+        <div className='back'>
+          {Object.prototype.hasOwnProperty.call(card, 'img')
+            ? <img src={`./img/${props.folder}/${card.img}`} className='card-img' alt={card.img} />
+            : <div className='card-txt'><h2>{card.txt}</h2><p>{card.helpTxt}</p></div>}
         </div>
       </div>
-    );
+    </div>
+  )
 }
 
-export default Card;
+export default Card
 
 Card.propTypes = {
   card: PropTypes.object.isRequired
