69ad584c363d0fbcdeefe5c3b933fca942a63978
Efua Akumanyi <efua.akumanyi@gmail.com>
2021-03-20 15:13:55 +0000

    Setup depandabot

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
+import fixture from './models/dt.json'
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+test('renders game title', () => {
+  const { getByText } = render(<App />)
+  expect(getByText(fixture.name)).toBeInTheDocument()
 })
